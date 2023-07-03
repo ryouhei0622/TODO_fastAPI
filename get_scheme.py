c05c452c327d554d257cd290e07bc0c39efe0ad7
ryouhei0622 <seijakunomutou@gmail.com>
2023-07-03 19:27:50 +0900

    refac

--- get_scheme.py
+++ get_scheme.py
@@ -1,10 +1,9 @@
 import json
 
-import requests
 import yaml
 
 from api.main import app
 
 yaml_schema = yaml.dump(app.openapi(), Dumper=yaml.CDumper)
-with open("openapi_schema_raw.yaml", "w") as file:
+with open("openapi_schema.yaml", "w") as file:
     file.write(yaml_schema)
