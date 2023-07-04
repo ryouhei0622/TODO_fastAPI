01f6336b1b905e75b1d448c6227a15b0c9d8e185
ryouhei0622 <seijakunomutou@gmail.com>
2023-07-04 13:44:16 +0900

    tes用に変更

--- get_schema_remain_order.py
+++ get_schema_remain_order.py
@@ -3,5 +3,5 @@ from ruamel.yaml import YAML
 from api.main import app
 
 yaml = YAML()
-with open("openapi_schema.yaml", "w") as file:
+with open("openapi_schema_test.yaml", "w") as file:
     yaml.dump(app.openapi(), file)
