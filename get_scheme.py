bb398bf8ceafda3eaed9e369497f5be13637b028
ryouhei0622 <seijakunomutou@gmail.com>
2023-07-04 11:09:21 +0900

    コメント

--- get_scheme.py
+++ get_scheme.py
@@ -9,3 +9,15 @@ from api.main import app
 # これで行けた
 with open("openapi_schema.yaml", "w") as file:
     yaml.dump(app.openapi(), file, Dumper=yaml.CDumper)
+
+# versionの明記があるはず
+# エンコードがうまくいかない
+
+# なければデフォルトにする
+# openapi: "3.0.3"
+# info:
+#   version: 0.1.0
+#   title: FastPCD
+#   description: A cloud server that helps akari members' developments
+
+#
