87a77343cf660edb48b9e1b9ba3b79946189c7e1
ryouhei0622 <seijakunomutou@gmail.com>
2023-07-04 12:00:07 +0900

    なんやかんや試した

--- get_scheme.py
+++ get_scheme.py
@@ -1,4 +1,5 @@
-import yaml
+# import yaml
+from ruamel.yaml import YAML
 
 from api.main import app
 
@@ -7,8 +8,15 @@ from api.main import app
 #     file.write(yaml_schema)
 
 # これで行けた
+yaml = YAML()
+# with open("openapi_schema_random.yaml", "w") as file:
+# yaml.dump(
+#     app.openapi(), file, Dumper=yaml.CDumper, default_flow_style=False, encoding=("utf-8"), allow_unicode=True
+# )
+# yaml.dump(app.openapi(), file)
 with open("openapi_schema.yaml", "w") as file:
-    yaml.dump(app.openapi(), file, Dumper=yaml.CDumper)
+    yaml.dump(app.openapi(), file)
+    # yaml.dump({"a": [1, 2]}, file)
 
 # versionの明記があるはず
 # エンコードがうまくいかない
@@ -21,3 +29,10 @@ with open("openapi_schema.yaml", "w") as file:
 #   description: A cloud server that helps akari members' developments
 
 #
+# import json
+
+# schema = json.dumps(app.openapi())
+# yaml_schema = yaml.dump(schema)
+# # yaml_schema = yaml.dump(app.openapi(), Dumper=yaml.CDumper)
+# with open("openapi_schema_raw.yaml", "w") as file:
+#     file.write(yaml_schema)
